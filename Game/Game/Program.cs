19680a31bc15bbe9273b03373ebbe5ac27207f87
Amarnath Kolla <amarrama269@gmail.com>
2026-04-06 12:07:04 +0530

    first commit of UC1 code

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+namespace SnakeLadderGame
+{
+    enum GameOption
+    {
+        NoPlay = 0,
+        Ladder = 1,
+        Snake = 2
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int player1Pos = 0;   
+
+            Random rand = new Random();
+
+            Console.WriteLine("Snake & Ladder Game Started ");
+            Console.WriteLine($"Player starting position: {player1Pos}");
+        }
+    }
+}
