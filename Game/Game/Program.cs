5098318b90109bb81c259d0b2e53237537924d65
Amarnath Kolla <amarrama269@gmail.com>
2026-04-06 14:20:03 +0530

    added ther uc6 code

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -13,42 +13,52 @@ namespace SnakeLadderGame
     {
         static void Main()
         {
-            int player1Pos = 0;   
+            int player1Pos = 0;
+
+            int diceCount = 0;
 
             Random rand = new Random();
 
             Console.WriteLine("Snake & Ladder Game Started ");
             Console.WriteLine($"Player starting position: {player1Pos}");
 
-            int dice = rand.Next(1, 7);
-            Console.WriteLine($"Dice rolled: {dice}");
-
-            GameOption option = (GameOption)rand.Next(0, 3);
-
-            switch (option)
+            while (player1Pos < 100)
             {
-                case GameOption.NoPlay:
-                    Console.WriteLine("No Play");
-                    break;
+                int dice = rand.Next(1, 7);
+                diceCount++;
+                Console.WriteLine($"Dice rolled: {dice}");
 
-                case GameOption.Ladder:
-                    Console.WriteLine("Ladder");
-                    if (player1Pos + dice <= 100)   
-                        player1Pos += dice;
-                    break;
+                GameOption option = (GameOption)rand.Next(0, 3);
 
-                case GameOption.Snake:
-                    Console.WriteLine("Snake");
-                    player1Pos -= dice;             
-                    break;
+                switch (option)
+                {
+                    case GameOption.NoPlay:
+                        Console.WriteLine("No Play");
+                        break;
+
+                    case GameOption.Ladder:
+                        Console.WriteLine("Ladder");
+                        if (player1Pos + dice <= 100)
+                            player1Pos += dice;
+                        break;
+
+                    case GameOption.Snake:
+                        Console.WriteLine("Snake");
+                        player1Pos -= dice;
+                        break;
+                }
+
+                if (player1Pos < 0)
+                {
+                    player1Pos = 0;
+                }
+
+                Console.WriteLine($"Player position: {player1Pos}");
+
             }
+            Console.WriteLine("\n GAME OVER ");
+            Console.WriteLine($"Total Dice Rolls: {diceCount}");
 
-            if (player1Pos < 0)
-            {
-                player1Pos = 0;
-            }
-
-            Console.WriteLine($"Player position: {player1Pos}");
 
 
         }
